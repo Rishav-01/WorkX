73b438d518f7cb0b22408a01326842a7a31ead2b
Rishav <rishulal29@gmail.com>
2024-05-05 14:21:47 +0530

    completed scrolling

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,30 +1,48 @@
 import React from "react";
-import {
-  courseTwo,
-  courseOne,
-  courseThree,
-  courseFour,
-} from "../constants/index";
+import { courseData } from "../constants";
 import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
 
 const Cards = () => {
-  const slideLeft = () => {};
-  const slideRight = () => {};
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft -= 350;
+  };
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft += 350;
+  };
   return (
     <div className="flex flex-col items-center justify-center gap-2">
       {/* Slider  */}
-      <div className="flex gap-3">
-        <img src={courseOne} alt="" />
-        <img src={courseTwo} alt="" />
-        <img src={courseThree} alt="" />
-        <img src={courseFour} alt="" />
+      <div className="flex items-center relative">
+        <div
+          id="slider"
+          className="flex gap-2 w-[1000px] h-full overflow-hidden whitespace-nowrap scroll-smooth"
+        >
+          {courseData.map((item) => (
+            <img
+              src={item.img}
+              className="h-56 hover:scale-105 p-2 duration-200 cursor-pointer"
+              key={item.id}
+              alt="img"
+            />
+          ))}
+        </div>
       </div>
+
+      {/* Buttons  */}
       <div className="flex gap-2">
         <button onClick={slideLeft}>
-          <CiCircleChevLeft size={30} />
+          <CiCircleChevLeft
+            className="opacity-50 hover:opacity-100"
+            size={35}
+          />
         </button>
         <button onClick={slideRight}>
-          <CiCircleChevRight size={30} />
+          <CiCircleChevRight
+            className="opacity-50 hover:opacity-100"
+            size={35}
+          />
         </button>
       </div>
     </div>
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -4,4 +4,13 @@ import courseTwo from "/img/Course2.png";
 import courseThree from "/img/Course3.png";
 import courseFour from "/img/Course4.png";
 
-export { workXLogo, courseOne, courseThree, courseTwo, courseFour };
+const courseData = [
+  { id: 1, img: courseOne },
+  { id: 2, img: courseTwo },
+  { id: 3, img: courseThree },
+  { id: 4, img: courseFour },
+  { id: 5, img: courseOne },
+  { id: 6, img: courseTwo },
+];
+
+export { workXLogo, courseData };
