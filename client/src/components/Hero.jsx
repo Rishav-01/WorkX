23d59b52c24ac612c4f3ae4dc55a60cc2206e7e9
1Ashutosh1 <at8969927@gmail.com>
2024-05-06 00:22:20 +0530

    checking code structure

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import Cards from "./courses/Cards";
 
 const Hero = () => {
   return (
     <section className="mt-10 text-2xl font-semibold">
       <div className="flex gap-4 flex-col text-center">
-        <p>Hi, Rishav ! &#9996;</p>
+        <p>Hi, Jobseekers ! &#9996;</p>
         <p>Let's help you land your dream career !</p>
       </div>
       <p className="my-11 font-bold text-center">Trending on WorkX &#9889;</p>
