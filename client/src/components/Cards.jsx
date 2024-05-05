df6e5a4c6c31d4481ce8793c5e814a5c3f8aa619
Rishav <rishulal29@gmail.com>
2024-05-05 13:15:18 +0530

    added courses images

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -5,16 +5,27 @@ import {
   courseThree,
   courseFour,
 } from "../constants/index";
+import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
 
 const Cards = () => {
+  const slideLeft = () => {};
+  const slideRight = () => {};
   return (
-    <div className="flex items-center justify-center gap-5">
+    <div className="flex flex-col items-center justify-center gap-2">
       {/* Slider  */}
-      <div className="">
+      <div className="flex gap-3">
         <img src={courseOne} alt="" />
         <img src={courseTwo} alt="" />
         <img src={courseThree} alt="" />
         <img src={courseFour} alt="" />
+      </div>
+      <div className="flex gap-2">
+        <button onClick={slideLeft}>
+          <CiCircleChevLeft size={30} />
+        </button>
+        <button onClick={slideRight}>
+          <CiCircleChevRight size={30} />
+        </button>
       </div>
     </div>
   );
