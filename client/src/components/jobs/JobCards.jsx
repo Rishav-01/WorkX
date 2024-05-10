2285e9aaac4df821f3916c75c950406894dc555f
Rishav <rishulal29@gmail.com>
2024-05-10 11:14:55 +0530

    updated active states for job cards

--- client/src/components/jobs/JobCards.jsx
+++ client/src/components/jobs/JobCards.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import InternshipCards from "./InternshipCards";
 
 export default function JobCards() {
+  const [activeState, setActiveState] = useState("");
   return (
     <div className="mt-14">
       {/* Different Categories */}
@@ -9,28 +11,84 @@ export default function JobCards() {
           Popular categories:
         </h2>
         <div className="flex flex-wrap gap-4">
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("big-brands")}
+            className={`px-2 py-1 text-sm ${
+              activeState === "big-brands"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } rounded-full border `}
+          >
             Big brands
           </button>
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("work-from-home")}
+            className={`${
+              activeState === "work-from-home"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm rounded-full border`}
+          >
             Work from home
           </button>
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("part-time")}
+            className={`${
+              activeState === "part-time"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm rounded-full border`}
+          >
             Part-time
           </button>
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("mba")}
+            className={`${
+              activeState === "mba"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm  rounded-full border`}
+          >
             MBA
           </button>
-          <button className="px-2 py-1 text-sm text-blue-500 hover:text-blue-700 rounded-full border border-blue-500">
+          <button
+            onClick={() => setActiveState("engineering")}
+            className={`${
+              activeState === "engineering"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm rounded-full border`}
+          >
             Engineering
           </button>
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("media")}
+            className={`${
+              activeState === "media"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm rounded-full border`}
+          >
             Media
           </button>
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("design")}
+            className={`${
+              activeState === "design"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm rounded-full border`}
+          >
             Design
           </button>
-          <button className="px-2 py-1 text-sm text-gray-500 hover:text-gray-700 rounded-full border border-gray-300">
+          <button
+            onClick={() => setActiveState("data-science")}
+            className={`${
+              activeState === "data-science"
+                ? "text-blue-500 border-blue-500 hover:text-blue-700"
+                : "text-gray-500 hover:text-gray-700 border-gray-300"
+            } px-2 py-1 text-sm  rounded-full border`}
+          >
             Data Science
           </button>
         </div>
