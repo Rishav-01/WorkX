2bb04bbc6c4206cbfdb825981ca0a7ee89d75bfe
Rishav <rishulal29@gmail.com>
2024-10-05 16:12:04 +0530

    Fix

--- client/src/pages/Recruiter/Applicants.jsx
+++ client/src/pages/Recruiter/Applicants.jsx
@@ -71,7 +71,7 @@ const Applicants = () => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       {isLoading ? (
-        <div className="flex justify-center items-center w-full h-full flex-row gap-2">
+        <div className="flex justify-center items-center w-full h-screen flex-row gap-2">
           <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
           <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
           <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
